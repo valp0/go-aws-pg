10968b1d9b586821e1be425e81e4119d8a70626c
valp0 <ponch0_vb@hotmail.com>
2022-05-04 19:23:21 -0500

    NotFound
    Updated NotFound endpoint with authentication token endpint data.

--- handlers/notFound.go
+++ handlers/notFound.go
@@ -6,7 +6,7 @@ import (
 
 // NotFound will send a message inside a JSON containing the available endpoints.
 func (h handler) NotFound(w http.ResponseWriter, r *http.Request) {
-	message := `only available endpoints are GET /api/ping/, GET/POST/DELETE /api/users/, GET/PATCH /api/users/{id}, GET/POST /api/users/{id}/favorites/ and DELETE /api/users/{id}/favorites{vidId}/`
+	message := `only available endpoints are GET /api/ping/, GET/POST/DELETE /api/users/, GET/PATCH /api/users/{id}, GET/POST /api/users/{id}/favorites/, DELETE /api/users/{id}/favorites{vidId}/ and GET /auth/get-token/`
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusNotFound)
